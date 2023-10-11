649001020ea5b0fb19643d6b8f7989746b54e120
Scott R <86810816+xithical@users.noreply.github.com>
2023-10-11 15:55:00 -0500

    Update counter.js

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -3,7 +3,7 @@ var todayYY = todayRaw.getFullYear();
 var todayMM = todayRaw.getMonth();
 var todayDD = todayRaw.getDate();
 var today = new Date(todayYY, todayMM, todayDD);
-var incident = new Date(2023, 09, 09); //YYYY, MM, DD //month counts from 00 i.e may = 04 not 05
+var incident = new Date(2023, 09, 11); //YYYY, MM, DD //month counts from 00 i.e may = 04 not 05
 //var today = today.getTime();        // It seems you do not need this...? //
 //var incident = incident.getTime();  //                                   //
 
